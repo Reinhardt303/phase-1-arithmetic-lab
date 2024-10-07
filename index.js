7dcc79adc05dfc040a0243a7938dca2aa2a924d4
Thomas Rains <thomasrains01@gmail.com>
2024-10-07 14:30:09 -0700

    submits Phase 1 arithmatic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31
+const num2 = 2
+const num3 = 14
+const num4 = 5
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 100) + 1
+
+let mod = num3%num4
+
+const max = Math.max(1, 2, 3, 4, 20);
+
